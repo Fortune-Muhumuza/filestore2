a0d7610a97384ad4431468f49fca7714b07e9167
Fortune-Muhumuza <fortunemuhumuza@gmail.com>
2024-12-02 11:39:41 +0300

    feat(files): add filelist component

---
+++ src/app/components/FileList.tsx
@@ -0,0 +1,23 @@
+import { FileListProps } from "../../types/components";
+
+const FileList: React.FC<FileListProps> = ({ files, onSelect }) => {
+  return (
+    <div className="bg-gray-50 p-4 rounded-lg">
+      <ul>
+        {files.map((file, index) => (
+          <li
+            key={index}
+            className="py-2 px-3 border-b hover:bg-gray-100 cursor-pointer"
+            onClick={() => onSelect(file)}
+          >
+            {file.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default FileList;
+
+  
